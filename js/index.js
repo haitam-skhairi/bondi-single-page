fc131a8f5a9b5372002768d785c2fc9b4f1ce816
Haitam Skhairi <haitam.skhairi@gmail.com>
2022-12-23 00:27:37 +0100

    Add Fill animation in the features section

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// JavaScript File
+
+let features = document.querySelectorAll(".features .feat");
+let featCashed = document.querySelector(".features feat-cashed");
+let featMore = document.querySelector(".features .feat-more");
+
+featMore.addEventListener("click", function () {
+  console.log("it work");
+});
